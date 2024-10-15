35622c798192c8f575f7022386e22a8ef2913e5d
Alex Westgarth <alex.westgarth@acnc.gov.au>
2024-10-15 15:51:54 +1100

    Copied code, not yet tested

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,15 +8,37 @@ from count_pdfs import PDFCounter
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='PDF Preprocessing Script')
-    parser.add_argument('--input_folder', type=str, required=True, help='Path to the input folder containing PDFs')
-    parser.add_argument('--output_folder', type=str, required=True, help='Path to the output folder for preprocessed PDFs')
-    parser.add_argument('--statement_set_name', type=str, required=True, help='Name of the statement set')
+    parser = argparse.ArgumentParser(
+        description='''PDF Preprocessing Script.
+        
+        This script splits PDF files containing multiple statements into individual files to prepare them for processing. 
+        It creates a folder within the --input folder based on the --name given.
+        Within this folder it creates necessary folders, counts PDFs before and after splitting them, saves the counts to Excel files,
+        and stores the split files.''',
+        
+        epilog='''Exmaple: python src/preprocess.py --input PATH/TO/PDFS --name statement_run_1'''
+    )
+
+    
+    parser.add_argument(
+        '-i', '--input',
+        type=str,
+        required=True,
+        help='Path to the input folder containing PDFs that need to be split.'
+        )
+    
+    parser.add_argument(
+        '-n', '--name',
+        type=str,
+        required=True,
+        help='Name of the pre-process run - the folder created within the input folder will be named after this.'
+        )
+    
     args = parser.parse_args()
     
-    input_folder = args.input_folder
-    output_folder = args.output_folder
-    statement_set_name = args.statement_set_name
+    input_dir = args.input
+    output_folder = input_dir # Output folder for preprocessed PDFs will be created inside the given input folder
+    name = args.name
     
     # Create necessary folders
     env_prep = EnvironmentPrep()
@@ -25,7 +47,7 @@ def main():
         ready_for_analysis,
         manual_splitting_folder,
         analysed_files_folder,
-    ) = env_prep.create_folders(statement_set_name, output_folder)
+    ) = env_prep.create_folders(name, output_folder)
     
     # Count input PDFs
     pdf_counter = PDFCounter()
@@ -34,7 +56,7 @@ def main():
         summary_data_before,
         total_pre_files,
         total_pre_pages,
-    ) = pdf_counter.process_pdf_count([input_folder])
+    ) = pdf_counter.process_pdf_count([input_dir])
     
     # Save pre-split counts
     pdf_counter.save_to_excel(
@@ -44,7 +66,7 @@ def main():
     # Process PDFs
     pdf_processor = PDFProcessor()
     pdf_processor.process_all_pdfs(
-        input_folder,
+        input_dir,
         ready_for_analysis,
         manual_splitting_folder,
     )
